d001fa44d721183daf78c139af73ebd06a63b53f
Durann23 <139575570+Durann23@users.noreply.github.com>
2024-01-09 13:25:27 -0600

    Add files via upload

---
+++ JADR/Act1_JADR/Act1_JADR/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Act1_JADR
+//
+//  Created by Mac24 on 09/01/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
